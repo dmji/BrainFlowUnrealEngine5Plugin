602282e2be4c1f1ff21fbeb4971c5332a76156dd
daimonji <leyshkinanton@gmail.com>
2023-06-22 10:37:06 +0300

    update to unreal engine 5, restore compilation

--- BrainFlowPlugin/Source/BrainFlowPlugin/BrainFlowPlugin.Build.cs
+++ BrainFlowPlugin/Source/BrainFlowPlugin/BrainFlowPlugin.Build.cs
@@ -1,7 +1,6 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
 using UnrealBuildTool;
 using System.IO;
+using System.Collections.Generic;
 
 public class BrainFlowPlugin : ModuleRules
 {
@@ -13,71 +12,28 @@ public class BrainFlowPlugin : ModuleRules
 
         PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
 
-        if(Target.Platform == UnrealTargetPlatform.Win32)
+        if (Target.Platform == UnrealTargetPlatform.Win64)
         {
-            PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Compiled", "Win32_dynamic", "inc"));
-            PublicLibraryPaths.Add(Path.Combine(ModuleDirectory, "Compiled", "Win32_dynamic", "lib"));
-            PublicAdditionalLibraries.Add("Brainflow32.lib");
-            PublicAdditionalLibraries.Add("DataHandler32.lib");
-            PublicAdditionalLibraries.Add("BoardController32.lib");
-            PublicAdditionalLibraries.Add("MLModule32.lib");
-            RuntimeDependencies.Add("$(TargetOutputDir)/BoardController32.dll", Path.Combine(ModuleDirectory, "Compiled", "Win32_dynamic", "lib", "BoardController32.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/DataHandler32.dll", Path.Combine(ModuleDirectory, "Compiled", "Win32_dynamic", "lib", "DataHandler32.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/GanglionLib32.dll", Path.Combine(ModuleDirectory, "Compiled", "Win32_dynamic", "lib", "GanglionLib32.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/gforce32.dll", Path.Combine(ModuleDirectory, "Compiled", "Win32_dynamic", "lib", "gforce32.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/gForceSDKWrapper32.dll", Path.Combine(ModuleDirectory, "Compiled", "Win32_dynamic", "lib", "gForceSDKWrapper32.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/neurosdk-x86.dll", Path.Combine(ModuleDirectory, "Compiled", "Win32_dynamic", "lib", "neurosdk-x86.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/BrainBitLib32.dll", Path.Combine(ModuleDirectory, "Compiled", "Win32_dynamic", "lib", "BrainBitLib32.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/eego-SDK32.dll", Path.Combine(ModuleDirectory, "Compiled", "Win32_dynamic", "lib", "eego-SDK32.dll"));
-        }
-        if(Target.Platform == UnrealTargetPlatform.Win64)
-        {
-            PublicLibraryPaths.Add(Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib"));
-            PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "inc"));
-            PublicAdditionalLibraries.Add("Brainflow.lib");
-            PublicAdditionalLibraries.Add("DataHandler.lib");
-            PublicAdditionalLibraries.Add("BoardController.lib");
-            PublicAdditionalLibraries.Add("MLModule.lib");
-            RuntimeDependencies.Add("$(TargetOutputDir)/BoardController.dll", Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib", "BoardController.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/DataHandler.dll", Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib", "DataHandler.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/GanglionLib.dll", Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib", "GanglionLib.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/gforce64.dll", Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib", "gforce64.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/gForceSDKWrapper.dll", Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib", "gForceSDKWrapper.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/neurosdk-x64.dll", Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib", "neurosdk-x64.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/Unicorn.dll", Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib", "Unicorn.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/BrainBitLib.dll", Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib", "BrainBitLib.dll"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/eego-SDK.dll", Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic", "lib", "eego-SDK.dll"));
-        }
-        if(Target.Platform == UnrealTargetPlatform.Mac)
-        {
-            PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Compiled", "macos", "inc"));
-            PublicLibraryPaths.Add(Path.Combine(ModuleDirectory, "Compiled", "macos", "lib"));
-            PublicAdditionalLibraries.Add("libBrainflow.a");
-            PublicDelayLoadDLLs.Add("libDataHandler.dylib");
-            PublicDelayLoadDLLs.Add("libBoardController.dylib");
-            PublicDelayLoadDLLs.Add("libMLModule.dylib");
-            RuntimeDependencies.Add("$(TargetOutputDir)/libBoardController.dylib", Path.Combine(ModuleDirectory, "Compiled", "macos", "lib", "libBoardController.dylib"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/libDataHandler.dylib", Path.Combine(ModuleDirectory, "Compiled", "macos", "lib", "libDataHandler.dylib"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/libGanglionLib.dylib", Path.Combine(ModuleDirectory, "Compiled", "macos", "lib", "libGanglionLib.dylib"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/libMLModule.dylib", Path.Combine(ModuleDirectory, "Compiled", "macos", "lib", "libMLModule.dylib"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/libneurosdk-shared.dylib", Path.Combine(ModuleDirectory, "Compiled", "macos", "lib", "libneurosdk-shared.dylib"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/libBrainBitLib.dylib", Path.Combine(ModuleDirectory, "Compiled", "macos", "lib", "libBrainBitLib.dylib"));
-        }
-        if(Target.Platform == UnrealTargetPlatform.Linux)
-        {
-            PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Compiled", "linux", "inc"));
-            PublicLibraryPaths.Add(Path.Combine(ModuleDirectory, "Compiled", "linux", "lib"));
-            PublicAdditionalLibraries.Add("libBrainflow.a");
-            PublicDelayLoadDLLs.Add("libDataHandler.so");
-            PublicDelayLoadDLLs.Add("libBoardController.so");
-            PublicDelayLoadDLLs.Add("libMLModule.so");
-            RuntimeDependencies.Add("$(TargetOutputDir)/libBoardController.so", Path.Combine(ModuleDirectory, "Compiled", "linux", "lib", "libBoardController.so"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/libDataHandler.so", Path.Combine(ModuleDirectory, "Compiled", "linux", "lib", "libDataHandler.so"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/libGanglionLib.so", Path.Combine(ModuleDirectory, "Compiled", "linux", "lib", "libGanglionLib.so"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/libMLModule.so", Path.Combine(ModuleDirectory, "Compiled", "linux", "lib", "libMLModule.so"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/libunicorn.so", Path.Combine(ModuleDirectory, "Compiled", "linux", "lib", "libunicorn.so"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/libBrainBitLib.so", Path.Combine(ModuleDirectory, "Compiled", "linux", "lib", "libBrainBitLib.so"));
-            RuntimeDependencies.Add("$(TargetOutputDir)/libeego-SDK.so", Path.Combine(ModuleDirectory, "Compiled", "linux", "lib", "libeego-SDK.so"));
+            string compiledDir = Path.Combine(ModuleDirectory, "Compiled", "x64_dynamic");
+            string libDir = Path.Combine(compiledDir, "lib");
+
+            // lib's
+            List<string> aFileLib = new List<string>();
+            aFileLib.AddRange(new string[] { "BoardController.lib", "BrainBitLib.lib", "Brainflow.lib", "BrainFlowBluetooth.lib", "DataHandler.lib", "DSPFilters.lib", "GanglionLib.lib", "kissfft-double.lib", "MLModule.lib", "MuseLib.lib", "WaveLib.lib" });
+            aFileLib.ForEach(s => PublicAdditionalLibraries.Add(Path.Combine(libDir, s)));
+            //foreach (string s in aFileLib)
+            //    PublicAdditionalLibraries.Add(Path.Combine(libDir, s));
+
+            // dll's
+            string copyDllTo = Path.Combine(PluginDirectory, "Binaries", "Win64");
+            List<string> aFileDll = new List<string>();
+            aFileDll.AddRange(new string[] { "BoardController.dll", "BrainBitLib.dll", "BrainFlowBluetooth.dll", "DataHandler.dll", "eego-SDK.dll", "GanglionLib.dll", "MLModule.dll", "MuseLib.dll", "neurosdk-x64.dll", "Unicorn.dll" });
+            aFileDll.ForEach(s => RuntimeDependencies.Add($"$(TargetOutputDir)/{s}", Path.Combine(libDir, $"{s}")));
+            //foreach (string s in aFileDll)
+            //    RuntimeDependencies.Add(Path.Combine(copyDllTo, s), Path.Combine(libDir, $"{s}"));
+
+            // includes folder
+            PublicIncludePaths.Add(Path.Combine(compiledDir, "inc"));
         }
     }
 }
--- BrainFlowPlugin/Source/BrainFlowPlugin/Private/BrainFlowPlugin.cpp
+++ BrainFlowPlugin/Source/BrainFlowPlugin/Private/BrainFlowPlugin.cpp
@@ -4,7 +4,8 @@
 
 #define LOCTEXT_NAMESPACE "FBrainFlowPluginModule"
 
-void FBrainFlowPluginModule::StartupModule() {
+void FBrainFlowPluginModule::StartupModule() 
+{
   // This code will execute after your module is loaded into memory; the exact
   // timing is specified in the .uplugin file per-module
 }
